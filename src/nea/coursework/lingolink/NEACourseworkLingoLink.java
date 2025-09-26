52f68d46f6e05d4dfc7edd930ade8a52d03b75a2
4-narghirov <4-narghirov@BH-R20-18.local.gfmat.org>
2025-09-26 10:56:24 +0100

    This commit contains x y and z

--- src/nea/coursework/lingolink/NEACourseworkLingoLink.java
+++
@@ -1,9 +0,0 @@
-package nea.coursework.lingolink;
-
-public class NEACourseworkLingoLink {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-    
-}
